0074086976bf922e01d34e10d3d7733c1447d916
pkaislan123 <comprasaislan@gmail.com>
2021-12-19 13:00:00 -0300

    Commit Inicial

---
+++ GerandoRenda/src/main/java/br/com/gerandorendaapi/GerandoRendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerandorendaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerandoRendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerandoRendaApplication.class, args);
+	}
+
+}
---
+++ GerandoRenda/src/test/java/br/com/gerandorendaapi/GerandoRendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerandorendaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerandoRendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
