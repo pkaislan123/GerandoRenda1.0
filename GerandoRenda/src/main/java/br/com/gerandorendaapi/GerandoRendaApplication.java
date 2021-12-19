76691d0bad0159b88807225f0cdc94c55eaea56f
pkaislan123 <comprasaislan@gmail.com>
2021-12-19 14:21:48 -0300

    Commit de Teste

--- GerandoRenda/src/main/java/br/com/gerandorendaapi/GerandoRendaApplication.java
+++ GerandoRenda/src/main/java/br/com/gerandorendaapi/GerandoRendaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GerandoRendaApplication {
-
+//teste2.0
 	public static void main(String[] args) {
 		SpringApplication.run(GerandoRendaApplication.class, args);
 	}
